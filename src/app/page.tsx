89a4cb435693236af90edd64758a4d7ec7197bff
YumpA <russki-misha@mail.ru>
2025-02-04 19:53:38 +0500

    add fontsSize, spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const text2 = `
 `;
 
 const text3 = `
- Лежал на ботлайне пока тима брала нексус
+ Был номинирован на "Премию ректора" за творческие успехи
 `;
 
 const items: CollapseProps['items'] = [
@@ -43,19 +43,18 @@ const contentStyle: React.CSSProperties = {
   flexDirection:'row',
   height: '700px',
   color: '#fff',
-  textAlign: 'center',
-  background: 'rgb(26, 11, 46, 0.5)',
+  background: 'rgb(26, 11, 46, 0.8)',
   borderRadius:'10%',
 };
 
 export default function Home() {
   return (
-    <div style={{background: '#11071f'}}>
+    <div style={{background: 'linear-gradient(354deg, rgba(26,11,46,1) 0%, rgba(104,35,143,1) 38%, rgba(25,6,52,1) 85%)'}}>
       <header className="inset-x-0 top-0 z-50">
       <nav style={{background: '#1a0b2e'}}  className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div className="flex lg:flex-1">
         <a href="#" className="-m-1.5 p-1.5 text-xl font-bold text-white">
-          Y
+          <img src="image/Group 30.png" width={80} alt="logo"></img>
         </a>
       </div>
       <div className="flex lg:hidden">
@@ -66,11 +65,10 @@ export default function Home() {
           </svg>
         </button>
       </div>
-      <div className="hidden lg:flex lg:gap-x-12 " >
-      <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Главная</a>
-              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Обо мне</a>
-              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Опыт</a>
-              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Проекты</a>
+      <div className="hidden lg:flex lg:gap-x-12">
+              <a href="#about-me" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Обо мне</a>
+              <a href="#skills-projects" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Опыт и проекты</a>
+              <a href="#contacts" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Контакты</a>
       </div>
     </nav>
       </header>
@@ -78,11 +76,12 @@ export default function Home() {
         <div className="flex justify-center items-center p-20">
           <img src={"/image/meAndRector.png"} style={{maskImage: 'linear-gradient(to top, transparent 5%, #11071f)'}} width={400} alt="me" />
           <div>
-            <p className="text-white">Привет! Меня зовут <span className="text-purple-600">Александр</span></p>
-            <h1 className="text-white">Юный и <span className="text-purple-600">амбициозный</span> программист</h1>
+            <p className="text-white font-bold text-2xl mb-4">Привет! Меня зовут <span className="text-purple-600">Александр</span></p>
+            <p className="text-white font-bold text-2xl">Юный и <span className="text-purple-600">амбициозный</span> программист</p>
           </div>
         </div>
-        <div className="flex justify-center items-center" style={{minWidth:400}}>
+        <div className="flex flex-col justify-center items-center" style={{minWidth:400}}>
+          <h1 id="about-me" className="text-white mt-10 mb-10 font-bold text-xl">Обо мне</h1>
           <ConfigProvider theme={{
                           components: {
                           Collapse: {
@@ -96,12 +95,13 @@ export default function Home() {
           </ConfigProvider>
         </div>
 
+      <h1 id="skills-projects" className="text-white mt-20 mb-10 text-center font-bold text-xl">Опыт и проекты</h1>
       <Carousel  style={{width:1000, margin: '0 auto'}} autoplay>
         <div>
           <div style={contentStyle}>
-            <div>
-              <h3>Заголовок</h3>
-              <p>Краткое описание</p>
+            <div className="w-1/2 text-center">
+              <h3 className="text-xl font-bold">Monogame, Winforms, WPF</h3>
+              <p className="mt-10 mb-10 text-xl">Написал простую игру, разработал АСУ "Менеджер музыкальных групп" на C#</p>
               <Button>Подробнее</Button>
             </div>
             <div>
@@ -110,27 +110,46 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <h3 style={contentStyle}>2</h3>
+          <div style={contentStyle}>
+              <div className="w-1/2 text-center">
+                <h3 className="text-xl font-bold">Backend</h3>
+                <p className="mt-10 mb-10 text-xl">В команде разработали серверную часть для сайта чулочной фабрики</p>
+                <Button>Подробнее</Button>
+              </div>
+              <div>
+                <img src="/image/S600xU.webp" width={200} alt="nest"></img>
+              </div>
+            </div>
         </div>
         <div>
-          <h3 style={contentStyle}>3</h3>
+          <div style={contentStyle}>
+              <div className="w-1/2 text-center">
+                <h3 className="text-xl font-bold">VK mini app</h3>
+                <p className="mt-10 mb-10 text-xl">С командой разработали мини-приложение для обмена книгами</p>
+                <Button>Подробнее</Button>
+              </div>
+              <div>
+                <img src="/image/vkontakte.png" width={200} alt="vk"></img>
+              </div>
+            </div>
         </div>
         <div>
-          <h3 style={contentStyle}>4</h3>
+          <h3 style={contentStyle} className="text-xl">Скоро появятся ещё проекты</h3>
         </div>
     </Carousel>
     </main>
     <footer>
-      <h1 className="text-white">Контакты</h1>
+      <h1 id="contacts" className="text-white text-center mt-20 mb-20 font-bold text-xl">Контакты</h1>
       <Watermark content="YumpA" font={{color: "rgb(113, 39, 186, 0.2)"}}>
         <div className="flex flex-col">
-          <div>
-            <p className="text-white">Я нахожусь в активном поиске практики, стажировки или работы. Если я вас заинтересовал, напишите мне на почту</p>
-            <p className="text-white">russki-misha@mail.ru</p>
+          <div className="flex justify-center w-2/3 m-auto">
+            <p className="text-white text-xl mb-5">Я нахожусь в активном поиске практики, стажировки или работы. Если я вас заинтересовал, то ниже будут ссылки, где можно связаться со мной</p>
           </div>
-          <div>
-          <a href="#"><GoogleOutlined style={{fontSize: 50, color: '#7127BA'}} /></a>
-          <a href="#"><GithubOutlined style={{fontSize: 50, color:'#7127BA'}} /></a>
+          <div className="flex flex-row justify-around m-auto mt-6 mb-1 w-2/3">
+            <GoogleOutlined style={{fontSize: 50, color: '#7127BA', cursor:'pointer'}} />
+            <a href="https://github.com/YumpA"><GithubOutlined style={{fontSize: 50, color:'#7127BA'}} /></a>
+            <img style={{cursor:'pointer'}} src="image/icons8-телеграм-50.png" width={50} alt="tg"></img>
+            <img style={{cursor:'pointer'}} src="image/icons8-почта-50.png" width={50} alt="mail"></img>
           </div>
         </div>
       </Watermark>
